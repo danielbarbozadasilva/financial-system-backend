05fc7e353f3f1420d42e84cf7426859c2e765975
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-10 20:44:43 -0300

    chore: added model user

---
+++ db/migrations/20220519192345-user.js
@@ -0,0 +1,59 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('user', {
+      cod_user: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      cpf: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      gender: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      birth_date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      kind: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      address_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'address', key: 'cod_address' },
+        onDelete: 'CASCADE'
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('user')
+  }
+}
