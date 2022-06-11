2a54cdf04145b5aea623448ab4d632c845714f30
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-11 16:15:50 -0300

    feat: added error middleware

---
+++ api/utils/errors/errors.business_rule.js
@@ -0,0 +1,12 @@
+const status = require('http-status')
+const defaultMessage = 'A business mistake happened'
+const ErrorGeneric = require('./erros.generic_error')
+
+module.exports = class ErrorBusinessRule extends ErrorGeneric {
+  constructor (message) {
+    super(message)
+    Error.captureStackTrace(this, ErrorBusinessRule)
+    this.statusCode = status.BAD_REQUEST
+    this.message = message || defaultMessage
+  }
+}
