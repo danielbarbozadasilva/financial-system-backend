073af08a437eb934901b0e03b1ac4e4a61ceeb07
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-17 17:27:56 -0300

    chore: update city value

--- db/seeders/20220519200100-address.js
+++ db/seeders/20220519200100-address.js
@@ -8,28 +8,28 @@ module.exports = {
         {
           address: 'Rua ffg 123',
           uf: 'RJ',
-          city: 'Rio de janeiro',
+          city: 'CARAPEBUS',
           zip_code: '23200-200',
           complement: 'casa',
         },
         {
           address: 'Rua abc 123',
           uf: 'RJ',
-          city: 'Rio de janeiro',
+          city: 'CARMO',
           zip_code: '22133-100',
           complement: 'casa'
         },
         {
           address: 'Rua acr 123',
           uf: 'SP',
-          city: 'São Paulo',
+          city: 'ALTAIR',
           zip_code: '23233-200',
           complement: 'apart.'
         },
         {
           address: 'Rua gfgf 123',
           uf: 'SP',
-          city: 'São Paulo',
+          city: 'CAJAMAR',
           zip_code: '23233-200',
           complement: 'apart.'
         }
