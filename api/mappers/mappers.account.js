94ff464852ec6f86aac539592eabd333b5fd1408
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-21 21:00:04 -0300

    fix: fields mapper

--- api/mappers/mappers.account.js
+++ api/mappers/mappers.account.js
@@ -1,39 +1,45 @@
 const toDTOUserAssets = (model) => {
+  const date = new Date()
+
   return {
-    id: model.cod_user,
-    name: model.name,
-    email: model.email,
-    cpf: model.cpf,
-    transaction: model.map((item) => {
-      return {
-        id: item.cod_transaction,
-        total_quantity: item.total_quantity,
-        total_price: parseFloat(item.total_price).toLocaleString('pt-br', {
-          style: 'currency',
-          currency: 'BRL'
-        }),
-        id_asset:
-          item.transaction_details.financial_asset_catalog?.cod_fin_asset,
-        name: item.transaction_details.financial_asset_catalog?.name,
-        description:
-          item.transaction_details.financial_asset_catalog?.description,
-        bvmf: item.transaction_details.financial_asset_catalog?.bvmf,
-        current_price: parseFloat(
-          item.transaction_details.financial_asset_catalog?.current_price
-        ).toLocaleString('pt-br', {
-          style: 'currency',
-          currency: 'BRL'
-        }),
-        purchase_price: parseFloat(
-          item.transaction_details.purchase_price
-        ).toLocaleString('pt-br', {
-          style: 'currency',
-          currency: 'BRL'
-        })
-      }
-    })
+    id: model.cod_account,
+    type: model.type,
+    balance: model.balance,
+    open_date:
+      date.toLocaleDateString(model.user.birth_date) +
+      ' ' +
+      date.toLocaleTimeString('pt-BR'),
+    user_id: model.user_id,
+    bank_id: model.bank_id,
+    user: {
+      id: model.user.cod_user,
+      name: model.user.name,
+      email: model.user.email,
+      cpf: model.user.cpf,
+      gender: model.user.gender,
+      birth_date: model.user.birth_date,
+      password: model.user.password,
+      phone: model.user.phone,
+      status: model.user.status,
+      kind: model.user.kind,
+      created_at:
+        date.toLocaleDateString(model.user.created_at) +
+        ' ' +
+        date.toLocaleTimeString('pt-BR'),
+      updated_at:
+        date.toLocaleDateString(model.user.updated_at) +
+        ' ' +
+        date.toLocaleTimeString('pt-BR'),
+      address_id: model.user.address_id
+    },
+    bank: {
+      id: model.bank.cod_bank,
+      name: model.bank.name,
+      branch: model.bank.branch
+    }
   }
 }
+
 module.exports = {
   toDTOUserAssets
 }
