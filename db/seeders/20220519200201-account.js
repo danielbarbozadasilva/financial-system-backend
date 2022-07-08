e2dcbd1cbe77f9ddba85f90400745ef7aaeff49f
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-07-08 12:36:43 -0300

    refactor: update branch id

--- db/seeders/20220519200201-account.js
+++ db/seeders/20220519200201-account.js
@@ -5,30 +5,30 @@ module.exports = {
     await queryInterface.bulkInsert(
       'account',
       [
-        {
-          number: '21515371-0',
+        { 
+          number: '12316121-1',
           type: 'C/C',
           balance: 3100.00,
           user_id: 2,
-          branch_id: 2
+          branch_id: 1
         },
-        {
-          number: '48129171-3',
+        {  
+          number: '40126582-2',
           type: 'C/C',
           balance: 1100.00,
           user_id: 3,
-          branch_id: 3
+          branch_id: 1
         },
-        {
-          number: '42315371-5',
+        {  
+          number: '65536124-9',
           type: 'C/C',
           balance: 11400.00,
           user_id: 4,
-          branch_id: 4
+          branch_id: 1
         }
       ],
       {
-        updateOnDuplicate: ['type', 'balance', 'user_id', 'branch_id'],
+        updateOnDuplicate: ['number', 'type', 'balance', 'user_id', 'branch_id'],
         ignoreDuplicates: true
       }
     )
