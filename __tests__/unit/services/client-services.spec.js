100622ec3847e128446802eebdc1b9d40e28c509
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-27 09:12:34 -0300

    test: ensure updateClientService returns 200 on success

--- __tests__/unit/services/client-services.spec.js
+++ __tests__/unit/services/client-services.spec.js
@@ -22,6 +22,27 @@ describe('Client services', () => {
       const result = await services.changeStatusService(clientid, status)
       expect(result.success).toBe(true)
     })
+    test('Make sure updateClientService returns 200 on success', async () => {
+      const clientid = 3
+      const body = {
+        address: 'Rua abc, 123',
+        uf: 'RJ',
+        city: 'RIO DE JANEIRO',
+        zip_code: '23412-200',
+        complement: 'CASA',
+        cod_address: 1,
+        name: 'Daniel Barboza',
+        email: 'daniel@gmail.com',
+        cpf: '123.123.123-90',
+        gender: 'M',
+        kind: 'client',
+        birth_date: '09/09/2000',
+        password: 'daniel',
+        phone: '(21) 99878-9981'
+      }
+      const result = await services.updateClientService(clientid, body)
+      expect(result.success).toBe(true)
+    })
     test('Make sure listAllClientsService returns 500 if server error', async () => {
       await sequelize.close()
       try {
