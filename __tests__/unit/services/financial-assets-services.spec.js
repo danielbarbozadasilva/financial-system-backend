5e962f8dcf4a8093317075490f82297897a134a9
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-05 11:53:56 -0300

    test: ensure createFinancialAssetsService returns 500 if a server error occurs

--- __tests__/unit/services/financial-assets-services.spec.js
+++ __tests__/unit/services/financial-assets-services.spec.js
@@ -18,6 +18,27 @@ describe('Financial asset services', () => {
       expect(result.success).toBe(true)
     })
 
+    test('Make sure createFinancialAssetsService returns 200 on success', async () => {
+      const body = {
+        name: 'example',
+        description: 'description',
+        current_price: '31.99',
+        bvmf: 'MELI34',
+        quantity: '100',
+        image: {
+          type: 'image/jpeg',
+          origin: 'img01.jpg',
+          old_path:
+            'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg',
+          newName: 'img01.jpg',
+          new_path:
+            'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg'
+        }
+      }
+      const result = await services.createFinancialAssetsService(body)
+      expect(result.success).toBe(true)
+    })
+
     test('Make sure listFinancialAssetsService returns 500 if a server error occurs', async () => {
       try {
         await sequelize.close()
@@ -32,6 +53,30 @@ describe('Financial asset services', () => {
         await sequelize.close()
         const id = 1
         await services.listByIdFinancialAssetsService(id)
+      } catch (error) {
+        expect(error.statusCode).toBe(500)
+      }
+    })
+    test('Make sure createFinancialAssetsService returns 500 if a server error occurs', async () => {
+      try {
+        await sequelize.close()
+        const body = {
+          name: 'example',
+          description: 'description',
+          current_price: '31.99',
+          bvmf: 'MELI34',
+          quantity: '100',
+          image: {
+            type: 'image/jpeg',
+            origin: 'img01.jpg',
+            old_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg',
+            newName: 'img01.jpg',
+            new_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg'
+          }
+        }
+        await services.createFinancialAssetsService(body)
       } catch (error) {
         expect(error.statusCode).toBe(500)
       }
