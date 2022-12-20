dababc1086b6b7b5cba22dbc8c4f2ef54a27996e
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-20 18:03:01 -0300

    test: ensure /v1/client/:clientid returns 422 if the clientid is not valid

--- __tests__/integration/client-routes.test.js
+++ __tests__/integration/client-routes.test.js
@@ -8,34 +8,40 @@ describe('Client Routes', () => {
     await sequelize.close()
   })
 
-  // describe('Route GET /v1/client', () => {
-  //   test('Make sure /v1/client return 200 on client search', async () => {
-  //     const cpf = '413.423.614-41'
-  //     const result = await createCredentialService(cpf)
-  //     await request(app).get('/v1/client').set(result).expect(200)
-  //   })
-  //   test('Make sure /v1/client return 401 if user is not authenticated', async () => {
-  //     await request(app).get('/v1/client').expect(401)
-  //   })
-  //   test('Make sure /v1/client return 403 if user is not authorized', async () => {
-  //     const cpf = '233.113.223-35'
-  //     const result = await createCredentialService(cpf)
-  //     await request(app).get('/v1/client').set(result).expect(403)
-  //   })
-  // })
+  describe('Route GET /v1/client', () => {
+    test('Make sure /v1/client return 200 on client search', async () => {
+      const cpf = '413.423.614-41'
+      const result = await createCredentialService(cpf)
+      await request(app).get('/v1/client').set(result).expect(200)
+    })
+    test('Make sure /v1/client return 401 if user is not authenticated', async () => {
+      await request(app).get('/v1/client').expect(401)
+    })
+    test('Make sure /v1/client return 403 if user is not authorized', async () => {
+      const cpf = '233.113.223-35'
+      const result = await createCredentialService(cpf)
+      await request(app).get('/v1/client').set(result).expect(403)
+    })
+  })
 
-  // describe('Route GET /v1/client/:clientid', () => {
-  //   test('Make sure /v1/client/:clientid return 200 on client id search', async () => {
-  //     const cpf = '413.423.614-41'
-  //     const clientid = 1
-  //     const result = await createCredentialService(cpf)
-  //     await request(app).get(`/v1/client/${clientid}`).set(result).expect(200)
-  //   })
-  //   test('Make sure /v1/client/:clientid return 401 if user is not authenticated', async () => {
-  //     const clientid = 1
-  //     await request(app).get(`/v1/client/${clientid}`).expect(401)
-  //   })
-  // })
+  describe('Route GET /v1/client/:clientid', () => {
+    test('Make sure /v1/client/:clientid return 200 on client id search', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 1
+      const result = await createCredentialService(cpf)
+      await request(app).get(`/v1/client/${clientid}`).set(result).expect(200)
+    })
+    test('Make sure /v1/client/:clientid return 401 if user is not authenticated', async () => {
+      const clientid = 1
+      await request(app).get(`/v1/client/${clientid}`).expect(401)
+    })
+    test('Make sure /v1/client/:clientid returns 422 if the clientid is not valid', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 0
+      const result = await createCredentialService(cpf)
+      await request(app).get(`/v1/client/${clientid}`).set(result).expect(422)
+    })
+  })
 
   describe('Route PUT /v1/client/:clientid', () => {
     test('Make sure /v1/client/:clientid return 200 on update', async () => {
@@ -131,7 +137,7 @@ describe('Client Routes', () => {
         })
         .expect(401)
     })
-    test('Make sure /v1/client/:clientid returns 422 if the userid is not valid', async () => {
+    test('Make sure /v1/client/:clientid returns 422 if the clientid is not valid', async () => {
       const cpf = '413.423.614-41'
       const clientid = 0
       const result = await createCredentialService(cpf)
