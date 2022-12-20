bafeefb048f71d660c3094d3d1c43992ffcb3d4a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-20 17:13:40 -0300

    test: ensure /v1/client/:clientid return 401 if user is not authenticated

--- __tests__/integration/client-routes.test.js
+++ __tests__/integration/client-routes.test.js
@@ -8,7 +8,7 @@ describe('Client Routes', () => {
     await sequelize.close()
   })
 
-  describe('Route /v1/client', () => {
+  describe('Route GET /v1/client', () => {
     test('Make sure /v1/client return 200 on client search', async () => {
       const cpf = '413.423.614-41'
       const result = await createCredentialService(cpf)
@@ -21,6 +21,19 @@ describe('Client Routes', () => {
       const cpf = '233.113.223-35'
       const result = await createCredentialService(cpf)
       await request(app).get('/v1/client').set(result).expect(403)
+    })
+  })
+
+  describe('Route GET /v1/client/:clientid', () => {
+    test('Make sure /v1/client/:clientid return 200 on client id search', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 1
+      const result = await createCredentialService(cpf)
+      await request(app).get(`/v1/client/${clientid}`).set(result).expect(200)
+    })
+    test('Make sure /v1/client/:clientid return 401 if user is not authenticated', async () => {
+      const clientid = 1
+      await request(app).get(`/v1/client/${clientid}`).expect(401)
     })
   })
 })
