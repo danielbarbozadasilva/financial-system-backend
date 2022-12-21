3705f2be42bd9bb0df646ba4ea5e108ffc62a701
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-21 08:07:33 -0300

    test: ensure /v1/transaction/deposit/client/:clientid return 422 if client id is not valid

--- __tests__/integration/transaction-routes.test.js
+++ __tests__/integration/transaction-routes.test.js
@@ -34,9 +34,54 @@ describe('Transaction Routes', () => {
         .set(result)
         .expect(200)
     })
-    test('Make sure /v1/transaction/client return 401 if user is not authenticated', async () => {
+    test('Make sure /v1/transaction/client/:clientid return 401 if user is not authenticated', async () => {
       const clientid = 2
       await request(app).get(`/v1/transaction/client/${clientid}`).expect(401)
+    })
+    test('Make sure /v1/transaction/client/:clientid return 422 if client id is not valid', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 0
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .get(`/v1/transaction/client/${clientid}`)
+        .set(result)
+        .expect(422)
+    })
+  })
+
+  describe('Route /v1/transaction/deposit/client/:clientid', () => {
+    test('Make sure /v1/transaction/deposit/client/:clientid return 200 on transaction search', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 2
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .get(`/v1/transaction/deposit/client/${clientid}`)
+        .set(result)
+        .expect(200)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 401 if user is not authenticated', async () => {
+      const clientid = 2
+      await request(app)
+        .get(`/v1/transaction/deposit/client/${clientid}`)
+        .expect(401)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 403 if user is not authorized', async () => {
+      const cpf = '233.113.223-35'
+      const clientid = 2
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .get(`/v1/transaction/deposit/client/${clientid}`)
+        .set(result)
+        .expect(403)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 422 if client id is not valid', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 0
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .get(`/v1/transaction/client/${clientid}`)
+        .set(result)
+        .expect(422)
     })
   })
 })
