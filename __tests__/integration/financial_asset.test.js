f66087db2c104fac9c59b4c6b2a830e135641db7
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-21 07:35:07 -0300

    test: ensure /v1/financial/:financialid return 200 on update

--- __tests__/integration/financial_asset.test.js
+++ __tests__/integration/financial_asset.test.js
@@ -13,7 +13,7 @@ describe('Financial Routes', () => {
       await request(app).get('/v1/financial').expect(200)
     })
   })
-  
+
   describe('Route GET /v1/financial/:financialid', () => {
     test('Make sure /v1/financial/:financialid return 200 on financial id search', async () => {
       const cpf = '413.423.614-41'
@@ -34,6 +34,81 @@ describe('Financial Routes', () => {
       const result = await createCredentialService(cpf)
       await request(app)
         .get(`/v1/financial/${financialid}`)
+        .set(result)
+        .expect(422)
+    })
+  })
+
+  describe('Route PUT /v1/financial/:financialid', () => {
+    test('Make sure /v1/financial/:financialid return 200 on update', () => {
+      const cpf = '413.423.614-41'
+      const financialid = 1
+      const result = createCredentialService(cpf)
+      request(app)
+        .put(`/v1/financial/${financialid}`)
+        .send({
+          name: 'example',
+          description: 'description',
+          current_price: 31.99,
+          bvmf: 'MELI34',
+          quantity: 100,
+          image: {
+            type: 'image/jpeg',
+            origin: 'img01.jpg',
+            old_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg',
+            newName: 'img01.jpg',
+            new_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg'
+          }
+        })
+        .set(result)
+        .expect(200)
+    })
+    test('Make sure /v1/financial/:financialid returns 401 if the user is not authenticated', () => {
+      const financialid = 1
+      request(app)
+        .put(`/v1/financial/${financialid}`)
+        .send({
+          name: 'example',
+          description: 'description',
+          current_price: '31.99',
+          bvmf: 'MELI34',
+          quantity: '100',
+          image: {
+            type: 'image/jpeg',
+            origin: 'img01.jpg',
+            old_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg',
+            newName: 'img01.jpg',
+            new_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg'
+          }
+        })
+        .expect(401)
+    })
+    test('Make sure /v1/financial/:financialid returns 422 if the financialid is not valid', () => {
+      const cpf = '413.423.614-41'
+      const financialid = 0
+      const result = createCredentialService(cpf)
+      request(app)
+        .put(`/v1/financial/${financialid}`)
+        .send({
+          name: 'example',
+          description: 'description',
+          current_price: '31.99',
+          bvmf: 'MELI34',
+          quantity: '100',
+          image: {
+            type: 'image/jpeg',
+            origin: 'img01.jpg',
+            old_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg',
+            newName: 'img01.jpg',
+            new_path:
+              'C:\\Users\\pc01\\Desktop\\financial-system-backend\\api\\utils\\file\\financial\\img01.jpg'
+          }
+        })
         .set(result)
         .expect(422)
     })
