96032953026206e0287f3ba8e243b460446a45b5
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-21 07:53:07 -0300

    test: ensure /v1/transaction/client return 200 on transaction search

---
+++ __tests__/integration/transaction-routes.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const app = require('../../api/app')
+const { sequelize } = require('../../api/models/models.index')
+const { createCredentialService } = require('../../api/services/services.user')
+
+describe('Transaction Routes', () => {
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  describe('Route /v1/transaction/client', () => {
+    test('Make sure /v1/transaction/client return 200 on transaction search', async () => {
+      const cpf = '413.423.614-41'
+      const result = await createCredentialService(cpf)
+      await request(app).get('/v1/transaction/client').set(result).expect(200)
+    })
+    test('Make sure /v1/transaction/client return 401 if user is not authenticated', async () => {
+      await request(app).get('/v1/transaction/client').expect(401)
+    })
+    test('Make sure /v1/transaction/client return 403 if user is not authorized', async () => {
+      const cpf = '233.113.223-35'
+      const result = await createCredentialService(cpf)
+      await request(app).get('/v1/transaction/client').set(result).expect(403)
+    })
+  })
+})
