7d45d463ca62dad0c05578b32e2f66bb7424d505
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-21 08:18:08 -0300

    test: ensure /v1/transaction/deposit/client/:clientid return 200 on transaction create

--- __tests__/integration/transaction-routes.test.js
+++ __tests__/integration/transaction-routes.test.js
@@ -8,7 +8,7 @@ describe('Transaction Routes', () => {
     await sequelize.close()
   })
 
-  describe('Route /v1/transaction/client', () => {
+  describe('Route GET /v1/transaction/client', () => {
     test('Make sure /v1/transaction/client return 200 on transaction search', async () => {
       const cpf = '413.423.614-41'
       const result = await createCredentialService(cpf)
@@ -24,7 +24,7 @@ describe('Transaction Routes', () => {
     })
   })
 
-  describe('Route /v1/transaction/client/:clientid', () => {
+  describe('Route GET /v1/transaction/client/:clientid', () => {
     test('Make sure /v1/transaction/client return 200 on transaction search', async () => {
       const cpf = '413.423.614-41'
       const clientid = 2
@@ -49,7 +49,7 @@ describe('Transaction Routes', () => {
     })
   })
 
-  describe('Route /v1/transaction/deposit/client/:clientid', () => {
+  describe('Route GET /v1/transaction/deposit/client/:clientid', () => {
     test('Make sure /v1/transaction/deposit/client/:clientid return 200 on transaction search', async () => {
       const cpf = '413.423.614-41'
       const clientid = 2
@@ -80,6 +80,62 @@ describe('Transaction Routes', () => {
       const result = await createCredentialService(cpf)
       await request(app)
         .get(`/v1/transaction/client/${clientid}`)
+        .set(result)
+        .expect(422)
+    })
+  })
+
+  describe('Route POST /v1/transaction/deposit/client/:clientid', () => {
+    test('Make sure /v1/transaction/deposit/client/:clientid return 200 on transaction create', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 2
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .post(`/v1/transaction/deposit/client/${clientid}`)
+        .send({
+          origin_cpf: '139.345.567-90',
+          total: 2200.9,
+          bank_id: 1
+        })
+        .set(result)
+        .expect(200)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 401 if user is not authenticated', async () => {
+      const clientid = 2
+      await request(app)
+        .post(`/v1/transaction/deposit/client/${clientid}`)
+        .send({
+          origin_cpf: '139.345.567-90',
+          total: 2200.9,
+          bank_id: 1
+        })
+        .expect(401)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 403 if user is not authorized', async () => {
+      const cpf = '233.113.223-35'
+      const clientid = 2
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .post(`/v1/transaction/deposit/client/${clientid}`)
+        .send({
+          origin_cpf: '139.345.567-90',
+          total: 2200.9,
+          bank_id: 1
+        })
+        .set(result)
+        .expect(403)
+    })
+    test('Make sure /v1/transaction/deposit/client/:clientid return 422 if client id is not valid', async () => {
+      const cpf = '413.423.614-41'
+      const clientid = 0
+      const result = await createCredentialService(cpf)
+      await request(app)
+        .post(`/v1/transaction/deposit/client/${clientid}`)
+        .send({
+          origin_cpf: '139.345.567-90',
+          total: 2200.9,
+          bank_id: 1
+        })
         .set(result)
         .expect(422)
     })
