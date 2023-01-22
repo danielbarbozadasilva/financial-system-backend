cfcd5c3b90381cb2a8fa9cb3921b53b8ac24834f
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-22 04:55:54 -0300

    test: ensure checkBalanceService has the id property

--- __tests__/unit/services/account-services.spec.js
+++ __tests__/unit/services/account-services.spec.js
@@ -11,24 +11,34 @@ describe('Account services', () => {
       const result = await services.listAllAccountService()
       expect(result.success).toBe(true)
     })
+
     test('Make sure listAllAccountService has the id property', async () => {
       const result = await services.listAllAccountService()
       expect(result.data[0]).toHaveProperty('id')
     })
+
     test('Make sure listByIdAccountService return success', async () => {
       const id = 1
       const result = await services.listByIdAccountService(id)
       expect(result.success).toBe(true)
     })
+
     test('Make sure listByIdAccountService has the id property', async () => {
       const id = 1
       const result = await services.listByIdAccountService(id)
       expect(result.data).toHaveProperty('id')
     })
+
     test('Make sure checkBalanceService return success', async () => {
       const id = 2
       const result = await services.checkBalanceService(id)
       expect(result.success).toBe(true)
+    })
+
+    test('Make sure checkBalanceService has the id property', async () => {
+      const id = 2
+      const result = await services.checkBalanceService(id)
+      expect(result.data).toHaveProperty('id')
     })
   })
 })
