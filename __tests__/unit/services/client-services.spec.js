ff06dfb5fc47f3bc425b4c6ece1791bd3dfa0634
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-22 05:07:51 -0300

    test: ensure listAllClientsService has the id property

--- __tests__/unit/services/client-services.spec.js
+++ __tests__/unit/services/client-services.spec.js
@@ -11,6 +11,10 @@ describe('Client services', () => {
       const result = await services.listAllClientsService()
       expect(result.success).toBe(true)
     })
+    test('Make sure listAllClientsService has the id property', async () => {
+      const result = await services.listAllClientsService()
+      expect(result.data[0]).toHaveProperty('id')
+    })
     test('Make sure listByIdClientService return success', async () => {
       const id = 1
       const result = await services.listByIdClientService(id)
@@ -42,58 +46,6 @@ describe('Client services', () => {
       }
       const result = await services.updateClientService(clientid, body)
       expect(result.success).toBe(true)
-    })
-    test('Make sure listAllClientsService returns 500 if server error', async () => {
-      await sequelize.close()
-      try {
-        await services.listAllClientsService()
-      } catch (error) {
-        expect(error.statusCode).toBe(500)
-      }
-    })
-    test('Make sure listByIdClientService returns 500 if server error', async () => {
-      await sequelize.close()
-      try {
-        const id = 1
-        await services.listByIdClientService(id)
-      } catch (error) {
-        expect(error.statusCode).toBe(500)
-      }
-    })
-    test('Make sure changeStatusService returns 500 if server error', async () => {
-      await sequelize.close()
-      try {
-        const clientid = 2
-        const status = false
-        await services.changeStatusService(clientid, status)
-      } catch (error) {
-        expect(error.statusCode).toBe(500)
-      }
-    })
-    test('Make sure updateClientService returns 500 if server error', async () => {
-      await sequelize.close()
-      try {
-        const clientid = 2
-        const body = {
-          address: 'Rua abc, 123',
-          uf: 'RJ',
-          city: 'RIO DE JANEIRO',
-          zip_code: '23412-200',
-          complement: 'CASA',
-          cod_address: 1,
-          name: 'Daniel Barboza',
-          email: 'daniel@gmail.com',
-          cpf: '123.123.123-90',
-          gender: 'M',
-          kind: 'client',
-          birth_date: '09/09/2000',
-          password: 'daniel',
-          phone: '(21) 99878-9981'
-        }
-        await services.updateClientService(clientid, body)
-      } catch (error) {
-        expect(error.statusCode).toBe(500)
-      }
     })
   })
 })
