df293979e745de105a73092d56dd347b74d31946
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-25 18:08:56 -0300

    chore: separate rules from users

--- api/services/services.user.js
+++ api/services/services.user.js
@@ -10,41 +10,7 @@ const userMapper = require('../mappers/mappers.user')
 const ErrorGeneric = require('../exceptions/erros.generic-error')
 const ErrorNotAuthorized = require('../exceptions/errors.user-not-authorized')
 const ErrorNotAuthenticated = require('../exceptions/errors.user-not-authenticated')
-
-const profile = [
-  {
-    type: 1,
-    permission: [
-      'SEARCH_FINANCIAL',
-      'CREATE_FINANCIAL',
-      'DELETE_FINANCIAL',
-      'UPDATE_FINANCIAL',
-      'LIST_CLIENT',
-      'LIST_CLIENT_ID',
-      'UPDATE_STATUS_CLIENT',
-      'UPDATE_CLIENT',
-      'LIST_CLIENT_TRANSACTION',
-      'LIST_CLIENT_ID_TRANSACTION',
-      'CREATE_DEPOSIT',
-      'LIST_BANKS',
-      'LIST_CLIENT_BALANCE',
-      'LIST_ACCOUNT',
-      'LIST_ID_ACCOUNT',
-      'LIST_CLIENT_ID_DEPOSIT'
-    ]
-  },
-  {
-    type: 2,
-    permission: [
-      'SEARCH_FINANCIAL',
-      'CREATE_TRANSACTION',
-      'LIST_CLIENT_ID',
-      'LIST_CLIENT_BALANCE',
-      'LIST_CLIENT_ID_TRANSACTION',
-      'LIST_BANKS'
-    ]
-  }
-]
+const profile = require('../utils/utils.rules')
 
 const userIsValidService = async (cpf, password) => {
   const userDB = await user.findOne({
